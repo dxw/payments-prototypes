0c960196ae3df2db7dbc9cbe7922ea81764ceae2
Harry Trimble <harryroberttrimble@gmail.com>
2024-04-25 11:28:00 +0100

    Update complex cases

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,5 +5,17 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+  document.getElementById("showHide").onclick = function() {
+      var theDiv = document.getElementById("foo");
+      if(theDiv.style.display == 'none') {
+          theDiv.style.display = 'block';
+          this.innerHTML = 'Hide';
+      } else {
+          theDiv.style.display = 'none';
+          this.innerHTML = 'Show';
+      }
+  }
+
+  
 })
 
