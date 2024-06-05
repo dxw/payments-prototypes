5afb607c2edebd1362c718603ebd3fcd31a33626
Harry Trimble <harryroberttrimble@gmail.com>
2024-06-05 10:50:36 +0100

    Reorganise folders into prod and prototypes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,12 @@ window.GOVUKPrototypeKit.documentReady(() => {
           this.innerHTML = 'Show';
       }
   }
+
+  // input masking
+  $(document).ready(function(){
+    $('#birth-date').mask('00/00/0000');
+    $('#phone-number').mask('0000-0000');
+  });
   
 })
 
