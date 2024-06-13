f038f1a8cfde51d42d43b125d281c271adcba08e
Harry Trimble <harryroberttrimble@gmail.com>
2024-06-13 11:49:11 +0100

    Latest changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,10 +16,20 @@ window.GOVUKPrototypeKit.documentReady(() => {
       }
   }
 
-  // input masking
-  $(document).ready(function(){
-    $('#birth-date').mask('00/00/0000');
-    $('#phone-number').mask('0000-0000');
+  var phoneInput = document.getElementById('phone');
+  var myForm = document.forms.myForm;
+  var result = document.getElementById('result');  // only for debugging purposes
+
+  phoneInput.addEventListener('input', function (e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+  });
+
+  myForm.addEventListener('submit', function(e) {
+    phoneInput.value = phoneInput.value.replace(/\D/g, '');
+    result.innerText = phoneInput.value;  // only for debugging purposes
+    
+    e.preventDefault(); // You wouldn't prevent it
   });
   
 })
