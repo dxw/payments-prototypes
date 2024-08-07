a35f8efbea1dfac52e9419d98b35b5af91b46bcc
Harry Trimble <harryroberttrimble@gmail.com>
2024-08-07 14:27:25 +0100

    Add validation and input masking

--- app/routes.js
+++ app/routes.js
@@ -12,80 +12,24 @@ router.get("/", function (request, response) {
   response.redirect('/en/index')
 });
 
-// Filter out accommodation not in Wales
-router.post('/en/register/not-suitable-country', function(request, response) {
+// Check UTRN for is valid
+router.post('/en/prototype/v6/just-utrn-validated/invalid-utrn', function(request, response) {
 
-    var country = request.session.data['country']
-
-    if (country == "wls"){
-        response.redirect("/en/register/charge-fee");
-
-    } else if (country == "wls,eng"){
-        response.redirect("/en/register/charge-fee");
-
-    } else if (country == "wls,eng,sct"){
-        response.redirect("/en/register/charge-fee");
-
-    } else if (country == "wls,eng,sct,ni"){
-        response.redirect("/en/register/charge-fee");
-
-    } else if (country == "wls,sct"){
-        response.redirect("/en/register/charge-fee");
-
-    } else if (country == "wls,sct,ni"){
-        response.redirect("/en/register/charge-fee");
-
-    } else if (country == "wls,eng,ni"){
-        response.redirect("/en/register/charge-fee");
-
-    } else if (country == "wls,ni"){
-        response.redirect("/en/register/charge-fee");
-    
+    var utrn = request.session.data['utrn']
+    if (utrn == "2702 1339 4877"){
+        response.redirect("/en/prototype/v6/just-utrn-validated/amount")
     } else {
-        response.redirect("/en/register/not-suitable-country")
+        response.redirect("/en/prototype/v6/just-utrn-validated/invalid-utrn")
     }
 })
 
-// Filter out free accommodation 
-router.post('/en/register/not-suitable-fee', function(request, response) {
+// Check UTRN is valid
+router.post('/en/prototype/v6/show-amount/invalid-utrn', function(request, response) {
 
-    var fee = request.session.data['fee']
-    if (fee == "yes"){
-        response.redirect("/en/register/email")
+    var utrn = request.session.data['utrn']
+    if (utrn == "2702 1339 4877"){
+        response.redirect("/en/prototype/v6/show-amount/balance")
     } else {
-        response.redirect("/en/register/not-suitable-fee")
-    }
-})
-
-// Filter out free accommodation 
-router.post('/en/register/search-company-details', function(request, response) {
-
-    var OrgType = request.session.data['OrgType']
-    if (OrgType == "Charity"){
-        response.redirect("/en/register/search-charity-details")
-    } else {
-        response.redirect("/en/register/search-company-details")
-    }
-})
-
-// Stop non-Welsh postcodes being used
-router.post('/en/register/select-address', function(request, response) {
-
-    var postcode = request.session.data['postcode']
-    if (postcode == "HR1 1DB"){
-        response.redirect("/en/register/non-welsh-postcode")
-    } else {
-        response.redirect("/en/register/select-address")
-    }
-})
-
-// Choose how to register - form or spreadsheet
-router.post('/en/register/postcode', function(request, response) {
-
-    var inputtype = request.session.data['inputtype']
-    if (inputtype == "spreadsheet"){
-        response.redirect("/en/register/upload-spreadsheet")
-    } else {
-        response.redirect("/en/register/postcode")
+        response.redirect("/en/prototype/v6/show-amount/invalid-utrn")
     }
 })
